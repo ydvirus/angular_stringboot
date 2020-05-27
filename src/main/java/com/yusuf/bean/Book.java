00a36d50490d00792e347da459c1a09b615a72ff
Yusuf Dalal <yusufkdalal123@gmail.com>
2020-05-27 19:21:05 +0530

    uploading springboot part in sub folder(SpringBoot)

---
+++ src/main/java/com/yusuf/bean/Book.java
@@ -0,0 +1,50 @@
+package com.yusuf.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+	
+	private int id;
+	private String name;
+	private int price;
+	public Book(int id, String name, int price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+	public Book() {
+		super();
+	}
+	
+	
+
+	
+}
---
+++ src/main/java/com/yusuf/restapi/LibraryManagementApplication.java
@@ -0,0 +1,30 @@
+package com.yusuf.restapi;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yusuf.bean.*;
+
+@SpringBootApplication
+@RestController
+public class LibraryManagementApplication {
+
+	@CrossOrigin("*")
+	@GetMapping("/findAllBooks")
+	public List<Book> getBooks(){
+		return Stream.of(new Book(101,"java", 999),new Book(102,"python", 750),new Book(103,"c++", 900),
+				new Book(104,"javascript", 199)).collect(Collectors.toList());
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementApplication.class, args);
+	}
+
+}
